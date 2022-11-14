1d9df95106caadab6713ef08a63e8f0b4d54a7de
liangxiaojun <fatalxiao@163.com>
2022-11-14 14:18:59 +0800

    update observal_data.sql

--- db/observal_data.sql
+++ db/observal_data.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost_3306
- Source Server Type    : MySQL
- Source Server Version : 80031 (8.0.31)
- Source Host           : localhost:3306
- Source Schema         : labor_analgesia
-
- Target Server Type    : MySQL
- Target Server Version : 80031 (8.0.31)
- File Encoding         : 65001
-
- Date: 14/11/2022 14:16:36
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
